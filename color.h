a59cc12d5696e4a3b9158d5577aaa63d95833a22
736E616B65 <66105371+736E616B65@users.noreply.github.com>
2020-06-02 18:32:41 +0900

    Create color.h

---
+++ color.h
@@ -0,0 +1,29 @@
+#ifndef __FONT_COLOR_H__
+#define __FONT_COLOR_H__
+
+#define BLACK           "\033[0;30m%s\033[0m"
+#define RED             "\033[0;31m%s\033[0m"
+#define GREEN           "\033[0;32m%s\033[0m"
+#define YELLOW          "\033[0;33m%s\033[0m"
+#define BLUE            "\033[0;34m%s\033[0m"
+#define PURPLE          "\033[0;35m%s\033[0m"
+#define CYAN            "\033[0;36m%s\033[0m"
+#define WHITE           "\033[0;37m%s\033[0m"
+
+#define DGRAY           "\033[1;30m%s\033[0m"
+#define DRED            "\033[1;31m%s\033[0m"
+#define DGREEN          "\033[1;32m%s\033[0m"
+#define DYELLW          "\033[1;33m%s\033[0m"
+#define DBLUE           "\033[1;34m%s\033[0m"
+#define DMAGENTA        "\033[1;35m%s\033[0m"
+ 
+#define BBLACK          "\033[0;40m%s\033[0m"
+#define BRED            "\033[0;41m%s\033[0m"
+#define BGREEN          "\033[0;42m%s\033[0m"
+#define BBROWN          "\033[0;43m%s\033[0m"
+#define BBLUE           "\033[0;44m%s\033[0m"
+#define BMAGENTA        "\033[0;45m%s\033[0m"
+#define BCYAN           "\033[0;46m%s\033[0m"
+#define BLIGHT          "\033[0;47m%s\033[0m"
+
+#endif
